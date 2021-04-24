3106908b84e83ba73bccc2015aafaecaf1ee4903
ShunjiYamanaka <syun.singernet@gmail.com>
2021-04-24 17:54:01 +0900

    Create MemoDetailScreen

--- App.jsx
+++ App.jsx
@@ -1,27 +1,9 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
 
-// components
-import AppBar from "./src/components/AppBar";
-import MemoList from "./src/components/MemoList";
-import CicleButton from "./src/components/CircleButton";
+// screen
+import MemoListScreen from "./src/screens/MemoListScreen";
+import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      {/* Header */}
-      <AppBar />
-      {/* Main */}
-      <MemoList />
-      {/* Add Btn */}
-      <CicleButton>+</CicleButton>
-    </View>
-  );
+  return <MemoDetailScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F0F4F8",
-  },
-});
---
+++ src/screens/MemoListScreen.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+// components
+import AppBar from "../components/AppBar";
+import MemoList from "../components/MemoList";
+import CicleButton from "../components/CircleButton";
+
+export default function MemoListScreen() {
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      <AppBar />
+      {/* Main */}
+      <MemoList />
+      {/* Add Btn */}
+      <CicleButton>+</CicleButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F0F4F8",
+  },
+});
